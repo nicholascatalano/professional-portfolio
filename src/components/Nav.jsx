98aeb04d026cca74e1c1c756ef69013dcc3da449
nickcatalano5 <nickcatalano24@gmail.com>
2024-02-01 17:31:29 -0500

    adds Nav.jsx to render Nav bar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Nav(props) {
+  const navTabs = ["About", "Portfolio", "Contact", "Resume"];
+  return (
+    <div className="tabs is-centered">
+      <ul className="nav nav-tabs">
+        {navTabs.map((navTab) => (
+          <li
+            className={
+              props.currentPage === navTab ? "nav-item is-active" : "nav-item"
+            }
+            key={navTab}
+          >
+            <a
+              href={"#" + navTab.toLowerCase()}
+              // Whenever a tab is clicked on,
+              // the current page is set through the handlePageChange props.
+              onClick={() => props.handlePageChange(navTab)}
+              className={
+                props.currentPage === navTab ? "nav-link active" : "nav-link"
+              }
+            >
+              {navTab}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
