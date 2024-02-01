0e5eee9ab69ccf63bdf2c4d46ad87b59ad0646bb
nickcatalano5 <nickcatalano24@gmail.com>
2024-02-01 17:10:45 -0500

    adds About page that loads automatically

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <p className="content is-medium">Full Stack Web Developer</p>
+      <hr />
+      <img className="my-pic" src="/images/pfp.jpg" alt="Image of myself" />
+      <p className="content is-italic mt-4">
+        My name is Nick Catalano, and I am a student of the Columbia University
+        Full Stack Coding Bootcamp.
+      </p>
+      <p className="content"></p>
+    </div>
+  );
+}
+
+export default About;
