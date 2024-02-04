d0d53364aeffb66af836e247d4ec84c246508be9
nickcatalano5 <nickcatalano24@gmail.com>
2024-02-04 15:19:38 -0500

    Imports download icon from react-icons, adds download button to /Resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BsCloudDownload } from "react-icons/bs";
 
 function Resume() {
   return (
@@ -6,6 +7,15 @@ function Resume() {
       <div className="column">
         <p className="content is-medium">Resume</p>
         <hr />
+        <p className="content is-italic mt-4">Click to Download</p>
+        <a
+          className="button is-primary"
+          href={"/images/NicholasCatalano.Resume.pdf"}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsCloudDownload />
+        </a>
         <iframe
           src="/images/NicholasCatalano.Resume.pdf"
           width="1000px"
