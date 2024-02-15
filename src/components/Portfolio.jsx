2178c382b996119a3c775286c41a15d879401f7e
nickcatalano5 <nickcatalano24@gmail.com>
2024-02-15 15:04:00 -0500

    updates portfolio page with new app

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,6 +4,18 @@ import Project from "./Project";
 const projects = [
   {
     id: 0,
+    title: "Fresh Finder",
+    image: "/images/fresh_finder.png",
+    languages: "react, html, javascript, css",
+    packages:
+      "react, mongodb, @apollo/server, graphql, image-to-base64, express, jwt",
+    description:
+      "Fresh Finder is a peer to peer web application designed to connect local farmers and food producers with health and environmentally focused consumers.",
+    repo: "https://github.com/YuliyaTsoy/food-marketplace-app",
+    live: "https://market-place-app.onrender.com/",
+  },
+  {
+    id: 1,
     title: "Foodie Finds App",
     image: "/images/foodie-finds.png",
     languages: "html, css, javascript, handlebars",
@@ -15,7 +27,7 @@ const projects = [
     live: "https://restaurant-blog-3f12d3a2bc88.herokuapp.com/",
   },
   {
-    id: 1,
+    id: 2,
     title: "Book Organizer",
     image: "/images/book-organizer.png",
     languages: "html, css, javascript",
@@ -24,30 +36,7 @@ const projects = [
     description:
       "The all in one place for book researchers and readers, that allows for the creation of your own library that you can revisit whenever you want. Our motivation was to develop an app that allows users to discover, explore, and organize their favorite books and other valuable information.",
     repo: "https://github.com/nicholascatalano/book-organizer",
-    live: "",
-  },
-  {
-    id: 2,
-    title: "J.A.T.E, Text Editor",
-    image: "/images/text-editor.png",
-    languages: "html, css, javascript",
-    packages:
-      "express, babel-loader, css-loader, html-webpack-plugin, http-server, style-loader, webpack, webpacl-cli, webpack-dev-server, webpack-pwa-manifest, workbox-webpack-plugin",
-    description:
-      "J.A.T.E is a Progressive Web Application (PWA) that runs in the browser, offline and can be installed locally to your machine. This application features a number of data persistence techniques, insuring the application runs regardless of browser supported function. J.A.T.E uses an IndexedDB database and the idb package.",
-    repo: "https://github.com/nicholascatalano/text-editor",
-    live: "https://text-editor-cu-e0ae20bad742.herokuapp.com/",
-  },
-  {
-    id: 3,
-    title: "Note Taker",
-    image: "/images/note-taker-app.png",
-    languages: "html, css, javascript",
-    packages: "express, uuid",
-    description:
-      "This is an application that allows the user to create, save, and delete notes. The application uses Express.js in the backend to retrieve and save the created notes to a JSON file. The application was deployed using Heroku.",
-    repo: "https://github.com/nicholascatalano/note-taker",
-    live: "https://note-taker-cu-3e28c82291c5.herokuapp.com/",
+    live: "https://book-researcher-and-organizer.onrender.com/",
   },
 ];
 
