8c25b4545f0833951b799a3a171e0b60ad50bda2
nickcatalano5 <nickcatalano24@gmail.com>
2024-03-04 11:58:17 -0500

    adjusts About page description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function About() {
       <hr />
       <img className="my-pic" src="/images/pfp.jpg" alt="Image of myself" />
       <p className="content is-italic mt-4">
-        My name is Nicholas (Nick) Catalano, and I am a student of the Columbia
+        My name is Nicholas (Nick) Catalano, and I am a graduate of the Columbia
         University Full Stack Coding Bootcamp. This is my Professional Portfolio
         of projects that I have worked on and developed.
       </p>
